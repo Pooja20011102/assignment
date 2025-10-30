4371371d4d2934193306c550cdea29f5f8561d62
selvija44 <145205633+selvija44@users.noreply.github.com>
2025-10-30 10:08:51 +0530

    done

---
+++ Day 19/task10.js
@@ -0,0 +1,7 @@
+for (let i = 1;i<=20;i++){
+    console.log(i);//prints 10
+    if(i==10){
+        break;//loop break when it reaches 10
+    }
+    // console.log(i);
+}
---
+++ Day 19/task6.js
@@ -0,0 +1,4 @@
+for (let i = 1;i<=10;i++){
+    res = `5 X ${i} = ${5*i}`;
+    console.log(res);
+}
