cf19f57e72ed6f98e5a0105580a7859751f03815
selvija44 <145205633+selvija44@users.noreply.github.com>
2025-11-08 17:08:37 +0530

    done

---
+++ Day 24/task6.js
@@ -0,0 +1,8 @@
+const a = document.getElementById('text');
+const b = document.getElementById('number');
+const c = document.getElementById('email');
+
+console.log("Hello fhhg");
+
+
+console.log(a);
